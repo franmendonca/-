5453f56065c2a2998c24cde0bad2125fb92596bd
Francisco Mendonca <franciscomendonca8@gmail.com>
2022-03-29 15:04:01 +0100

    added styling and subject to contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,12 +3,13 @@ class Contact < MailForm::Base
   attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message, validate: true
   attribute :nickname, captcha: true
+  attribute :subject, validate: true
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "Contact Form Inquiry",
+      :subject => "#{subject}",
       :to => "franciscomendonca8@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
